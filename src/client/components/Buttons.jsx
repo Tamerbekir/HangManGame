5b7f6b1f4b9bee15e8219accaa19ba4c9beed87e
Tamerbekir <astoryofame@gmail.com>
2024-10-20 01:11:01 -0400

    Once a letter is picked by the user the button will go away and the letter will appear on the screen as used

--- src/client/components/Buttons.jsx
+++ src/client/components/Buttons.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 const Buttons = () => {
 
   const [alphabet, setAlphabet] = useState('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
+  const [buttonSelected, setButtonSelected] = useState([])
 
   const handleLetters = (selection) => {
     setAlphabet(alphabet.filter((letter) => letter !== selection))
+    setButtonSelected([...buttonSelected, selection].join(' '))
   }
+
 
 
   return (
@@ -14,6 +17,10 @@ const Buttons = () => {
       {alphabet.map((letter, index) => (
         <button key={index} onClick={() => handleLetters(letter)}>{letter}</button>
       ))}
+      <div>
+        <h3>Selected Letters</h3>
+        <p>{buttonSelected}</p>
+      </div>
     </div >
   )
 }
